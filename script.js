3e82d189dc6f78801da03b5f97e8d10e896179f6
DeveloperManikDas <developermanikdas@gmail.com>
2024-02-03 11:19:13 +0530

    navbar complete

---
+++ script.js
@@ -0,0 +1,28 @@
+const navLinks = document.querySelectorAll("ul li");
+const workLink = document.getElementById("work");
+const activeTab = document.querySelector(".active-tab");
+
+activeTab.style.left = `${workLink.offsetLeft}px`;
+activeTab.style.width = `${workLink.offsetWidth}px`;
+
+window.addEventListener("scroll", checkScrollPosition);
+checkScrollPosition();
+
+function checkScrollPosition() {
+  const fixedNavbar = document.getElementById("fix-navbar");
+  const scrollThreshold = 40;
+  if (window.scrollY > scrollThreshold) {
+    fixedNavbar.classList.add("nav-scrolled");
+  } else {
+    fixedNavbar.classList.remove("nav-scrolled");
+  }
+}
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", function () {
+    navLinks.forEach((link) => link.classList.remove("active-tab"));
+    this.classList.add("active");
+    activeTab.style.left = this.offsetLeft + "px";
+    activeTab.style.width = this.offsetWidth + "px";
+  });
+});
